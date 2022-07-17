cb7c9792e6afbce904edffb659c3aafef8ba7ffc
ritosankho <93584116+ritosankho@users.noreply.github.com>
2022-07-17 16:35:21 +0530

    Add files via upload

---
+++ Login form.py
@@ -0,0 +1,23 @@
+from itertools import repeat
+import itertools
+num = 5
+for _ in itertools.repeat(None, num):
+    password = input("ENTER YOUR PASSWORD")
+    if password ==("abcd1234"):
+      print ("correct password")
+    else: print ("WORNG PASSWORD.")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
